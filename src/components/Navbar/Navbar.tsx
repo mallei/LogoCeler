76390ca2e2fd9e9cf98d731209645e5cb2b5b29d
mallei <235141495+mallei@users.noreply.github.com>
2026-06-27 15:47:24 +0200

    navbar aside

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,13 +6,21 @@ import {
   type Icon,
 } from "@tabler/icons-react";
 import classes from "./Navbar.module.css";
+import { useState } from "react";
 
 interface NavbarLinkProps {
   icon: Icon;
   label: string;
+  active: boolean;
+  onClick: () => void;
 }
 
-function NavbarLink({ icon: LinkIcon, label }: NavbarLinkProps) {
+function NavbarLink({
+  icon: LinkIcon,
+  label,
+  active,
+  onClick,
+}: NavbarLinkProps) {
   return (
     <Tooltip
       label={label}
@@ -20,7 +28,11 @@ function NavbarLink({ icon: LinkIcon, label }: NavbarLinkProps) {
       withArrow
       transitionProps={{ duration: 0 }}
     >
-      <UnstyledButton className={classes.link}>
+      <UnstyledButton
+        onClick={onClick}
+        className={classes.link}
+        data-active={active || undefined}
+      >
         <LinkIcon size={22} strokeWidth={1.5} />
       </UnstyledButton>
     </Tooltip>
@@ -28,21 +40,37 @@ function NavbarLink({ icon: LinkIcon, label }: NavbarLinkProps) {
 }
 
 export function Navbar() {
+  const [active, setActive] = useState(0);
+
   return (
     <nav className={classes.navbar}>
       <div className={classes.wrapper}>
         <div className={classes.aside}>
-          <Stack>
+          <Stack gap="xs">
             {[
               { icon: IconBrush, label: "Icon" },
               { icon: IconPalette, label: "Background" },
-            ].map((link) => (
-              <NavbarLink {...link} key={link.label} />
+            ].map((link, index) => (
+              <NavbarLink
+                {...link}
+                key={link.label}
+                active={index === active}
+                onClick={() => setActive(index)}
+              />
             ))}
           </Stack>
-          <NavbarLink icon={IconBrackets} label="About" />
+          <NavbarLink
+            icon={IconBrackets}
+            label="About"
+            active={2 === active}
+            onClick={() => setActive(2)}
+          />
         </div>
-        <div className={classes.main}>...</div>
+        <div className={classes.main}>
+          {active === 0 && <span>Icon</span>}
+          {active === 1 && <span>Background</span>}
+          {active === 2 && <span>About</span>}
+        </div>
       </div>
     </nav>
   );
